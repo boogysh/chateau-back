a34e8c81ae618638cdfe51b2543f060f33dfa1a3
Victor Buga <bugavictor86@gmail.com>
2023-02-06 23:00:39 +0100

    test27

--- app.js
+++ app.js
@@ -10,37 +10,22 @@ const msgRoutes = require("./routes/message");
 require("dotenv").config(); //or .config({ path:"../folder/.env"}), if .env is not a root file
 
 // //Connecting to mongoDB
-require("./connect_mongodb/mongodb"); // after dotenv
+//require("./connect_mongodb/mongodb"); // after dotenv
 
 //Parsing
 app.use(cors());
-app.use(express.json());   
+app.use(express.json());   //const bodyParser = require("body-parser");
 app.use(express.urlencoded({ extended: false }));
-            //const bodyParser = require("body-parser");
-
-//Setting CORS headers, to avoid CORS errors
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
+                                                    
 
 // //Setting routes
 
-app.use("/api/commandes", stuffRoutes);
-app.use("/api/messages", msgRoutes);
+// app.use("/api/commandes", stuffRoutes);
+// app.use("/api/messages", msgRoutes);
 
-// app.use("/", (req,res) => {
-//   res.json({message: "Hello from express"})
-// });
+app.use("/", (req,res) => {
+  res.json({message: "Hello from express"})
+});
 
 // const PORT = 4000;
 app.listen(process.env.PORT, (error) => {
@@ -63,6 +48,24 @@ app.listen(process.env.PORT, (error) => {
 // // const PORT = 4000;
 // app.listen(process.env.PORT, (error) => {
 //   error ? console.log(error) : console.log(`Server is running on port ${process.env.PORT}`);
+// });
+
+
+
+
+//Setting CORS headers, to avoid CORS errors
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
 // });
   
 
