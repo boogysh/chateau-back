083b580a25eb596088f53fa6ebf2dce1f624ec6f
Victor Buga <bugavictor86@gmail.com>
2023-02-06 22:05:38 +0100

    test14

--- app.js
+++ app.js
@@ -15,23 +15,24 @@ require("./connect_mongodb/mongodb"); // after dotenv
 app.use(express.json()); //const bodyParser = require("body-parser");
 
 //Setting CORS headers, to avoid CORS errors
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
+// });
 
 // //Setting routes
 
-app.use("/api/commandes", stuffRoutes);
-app.use("/api/messages", msgRoutes);
+// app.use("/api/commandes", stuffRoutes);
+// app.use("/api/messages", msgRoutes);
 
 app.use("/", (req,res) => {
   res.json({message: "Hello from express"})
