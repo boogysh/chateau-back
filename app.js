26c8d00fc72101efa703de533dffb76e34471e1d
Victor Buga <bugavictor86@gmail.com>
2023-02-06 22:55:45 +0100

    test25

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 //const Order = require("./models/Order");
 
 //requires routes
@@ -12,31 +13,33 @@ require("dotenv").config(); //or .config({ path:"../folder/.env"}), if .env is n
 require("./connect_mongodb/mongodb"); // after dotenv
 
 //Parsing
-app.use(express.json());               //const bodyParser = require("body-parser");
+app.use(cors());
+app.use(express.json());   
+            //const bodyParser = require("body-parser");
 
 //Setting CORS headers, to avoid CORS errors
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
+// });
 
 // //Setting routes
 
 app.use("/api/commandes", stuffRoutes);
 app.use("/api/messages", msgRoutes);
 
-app.use("/", (req,res) => {
-  res.json({message: "Hello from express"})
-});
+// app.use("/", (req,res) => {
+//   res.json({message: "Hello from express"})
+// });
 
 // const PORT = 4000;
 app.listen(process.env.PORT, (error) => {
