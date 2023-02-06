e73a9a0b9ba0580cc02b81fc1abc544e8417573f
Victor Buga <bugavictor86@gmail.com>
2023-02-06 22:47:59 +0100

    test24

--- app.js
+++ app.js
@@ -3,35 +3,35 @@ const app = express();
 //const Order = require("./models/Order");
 
 //requires routes
-//const stuffRoutes = require("./routes/stuff");
+const stuffRoutes = require("./routes/stuff");
 const msgRoutes = require("./routes/message");
 
 require("dotenv").config(); //or .config({ path:"../folder/.env"}), if .env is not a root file
 
 // //Connecting to mongoDB
-//require("./connect_mongodb/mongodb"); // after dotenv
+require("./connect_mongodb/mongodb"); // after dotenv
 
 //Parsing
 app.use(express.json());               //const bodyParser = require("body-parser");
 
 //Setting CORS headers, to avoid CORS errors
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 // //Setting routes
 
-// app.use("/api/commandes", stuffRoutes);
+app.use("/api/commandes", stuffRoutes);
 app.use("/api/messages", msgRoutes);
 
 app.use("/", (req,res) => {
