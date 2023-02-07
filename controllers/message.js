34556eb6304138b3df0a92db21d1732854354ab9
Victor Buga <bugavictor86@gmail.com>
2023-02-07 15:03:55 +0100

    t32

--- controllers/message.js
+++ controllers/message.js
@@ -1,13 +1,30 @@
 const MSG = require("../models/Message");
 
-exports.createMessage = (req, res) => {
+// exports.createMessage = (req, res) => {
+//   const { firstName, lastName, tel, email, messageTxt } = req.body;
+//   const message = new MSG({ firstName, lastName, tel, email, messageTxt });
+//   message
+//     .save()
+//     .then((message) => res.status(200).json(message))
+//     .catch((error) => handleError(res, error));
+// };
+
+exports.createMessage = async (req, res) => {
   const { firstName, lastName, tel, email, messageTxt } = req.body;
-  const message = new MSG({ firstName, lastName, tel, email, messageTxt });
-  message
-    .save()
-    .then((message) => res.status(200).json(message))
-    .catch((error) => handleError(res, error));
-};
+  try{
+    const message = await MSG.create({firstName, lastName, tel, email, messageTxt})
+    request.status(201).json(message);
+  }
+  catch(err){
+    console.error(err);
+  }
+}
+
+
+
+
+
+
 
 exports.getMessage = (req, res, next) => {
   MSG.find()
