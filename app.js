2c0b8d4cc550139ebb78dc9b4ab748918520fb14
Victor Buga <bugavictor86@gmail.com>
2023-02-07 09:25:38 +0100

    test33

--- app.js
+++ app.js
@@ -2,56 +2,47 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+//-------------------
+const MSG = require("./models/Message");
+//-------------------
 
 //requires routes
-const stuffRoutes = require("./routes/stuff");
-const msgRoutes = require("./routes/message");
+// const stuffRoutes = require("./routes/stuff");
+// const msgRoutes = require("./routes/message");
 
 require("dotenv").config(); //or .config({ path:"../folder/.env"}), if .env is not a root file
 
-// //Connecting to mongoDB
-require("./connect_mongodb/mongodb"); // after dotenv
+//Connecting to mongoDB
+//require("./connect_mongodb/mongodb"); // after dotenv
 
 //Parsing
-app.use(express.json());   //const bodyParser = require("body-parser");
+//app.use(express.json()); //const bodyParser = require("body-parser");
 app.use(cors());
 //app.use(express.urlencoded({ extended: false }));
-                                                    
 
 // //Setting routes
+//app.use("/api/commandes", stuffRoutes);
+//app.use("/api/messages", msgRoutes);
 
-app.use("/api/commandes", stuffRoutes);
-app.use("/api/messages", msgRoutes);
-
-// app.use("/", (req,res) => {
-//   res.json({message: "Hello from express"})
-// });
+app.use("/", (req, res) => {
+  res.json({ message: "Hello from express" });
+});
+app.get("/api/commandes", (req, res, next) => {
+  MSG.find()
+    .sort({ createdAt: -1 })
+    // .sort({ clientInfo: req.clientInfo })
+    .then((messages) => res.status(200).json(messages))
+    .catch((error) => res.status(400).json({ error }));
+});
 
 // const PORT = 4000;
 app.listen(process.env.PORT, (error) => {
-  error ? console.log(error) : console.log(`Server is running on port ${process.env.PORT}`);
+  error
+    ? console.log(error)
+    : console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 //---------------------------------------------------
-
-// const express = require("express");
-// const app = express();
-// //const Order = require("./models/Order");
-
-// require("dotenv").config();
-
-
-// app.use("/", (req,res) => {
-//   res.json({message: "Hello from express"})
-// });
-
-// // const PORT = 4000;
-// app.listen(process.env.PORT, (error) => {
-//   error ? console.log(error) : console.log(`Server is running on port ${process.env.PORT}`);
-// });
-
-
-
 
 //Setting CORS headers, to avoid CORS errors
 
@@ -67,6 +58,3 @@ app.listen(process.env.PORT, (error) => {
 //   );
 //   next();
 // });
-  
-
-
