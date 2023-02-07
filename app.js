2fa0b6f02127ee21340ce34e758b8307bfb6d00a
Victor Buga <bugavictor86@gmail.com>
2023-02-07 14:45:12 +0100

    t30

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 //const cors = require("cors");
 
 //requires routes
-// const stuffRoutes = require("./routes/stuff");
+const stuffRoutes = require("./routes/stuff");
 const msgRoutes = require("./routes/message");
 
 require("dotenv").config(); 
@@ -14,7 +14,7 @@ require("./connect_mongodb/mongodb");
 //app.use(cors());
 
 // //Setting routes
-//app.use("/api/commandes", stuffRoutes);
+app.use("/api/commandes", stuffRoutes);
 app.use("/api/messages", msgRoutes);
 
 app.listen(process.env.PORT, (error) => {
